442bea0fe7d17f8835c6e4db0e653ea3e99c524b
kohara2794 <keeganohara7@gmail.com>
2015-09-20 21:14:06 -0400

    Test commit for github

---
+++ Assignment 2.0/Select.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+ * Keegan O'Hara
+ * CS 148
+ * Assignment #2
+ * 9/17/15
+ */
+$query = 'SELECT * FROM ' . $tableName;
+    $info2 = $thisDatabaseReader->select($query, "", 0, 0, 0, 0, false, false);
+    $highlight = 0; // used to highlight alternate rows
+    foreach ($info2 as $rec) {
+        $highlight++;
+        if ($highlight % 2 != 0) {
+            $style = ' odd ';
+        } else {
+            $style = ' even ';
+        }
+        print '<tr class="' . $style . '">';
+        for ($i = 0; $i < $columns; $i++) {
+            print '<td>' . $rec[$i] . '</td>';
+        }
+        print '</tr>';
+    }
+    // all done
+    print '</table>';
+    print '</aside>';
+
